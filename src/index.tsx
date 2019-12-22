6d84e32aa6ce61f08d41cfbb2ab76bf76b785a47
Acidio Alan <acidio.alan@friday.de>
2019-12-22 23:50:57 +0100

    fix: vh unit fixed for mobile version

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Mobile devices have some differences when it goes for vh unit because their bars and so what.
+// One solution is to always consider the innerHeight as point of mesure
+function setVhProperty() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', setVhProperty);
+
+setVhProperty();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
